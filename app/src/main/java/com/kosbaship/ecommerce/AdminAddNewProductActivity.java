11ef45d6faa982c917368ffbeda79bdf2db9973f
mohamed <mohamed.kosba96@gmail.com>
2019-05-21 07:32:32 +0200

    Design activity_add_new_product

--- app/src/main/java/com/kosbaship/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/kosbaship/ecommerce/AdminAddNewProductActivity.java
@@ -2,6 +2,9 @@ package com.kosbaship.ecommerce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 // (10 - B - 6)
@@ -10,16 +13,30 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String CategoryName, Description, Price, Pname, saveCurrentDate, saveCurrentTime;
 
+    //(11 - B - 1)
+    // declare the views
+    private Button AddNewProductButton;
+    private ImageView InputProductImage;
+    private EditText InputProductName, InputProductDescription, InputProductPrice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
 
+        // (10 - B - 7)
+        // (11) Go to activity_admin_add_new_product
         // Receive the name of the category that send to us from
         // the admin category
         CategoryName = getIntent().getExtras().get("category").toString();
-        Toast.makeText(this, CategoryName, Toast.LENGTH_LONG).show();
 
+        //(11 - B - 2)
+        // get reference to the views
+        AddNewProductButton = findViewById(R.id.add_new_product);
+        InputProductImage = findViewById(R.id.select_product_image);
+        InputProductName = findViewById(R.id.product_name);
+        InputProductDescription = findViewById(R.id.product_description);
+        InputProductPrice = findViewById(R.id.product_price);
 
     }
 }
